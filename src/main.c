c5765acfe508b480239a6c4a655a0c4452798306
RenChan18 <renya.talacheva@gmail.com>
2025-03-24 16:32:47 +0300

    inaccuracies have been fixed. refactoring and improvements

--- src/main.c
+++ src/main.c
@@ -1,15 +1,14 @@
-#include <libopencm3/stm32/f4/rcc.h>
-#include <libopencm3/stm32/f4/gpio.h>
-#include <libopencm3/stm32/f4/i2c.h>
-#include <libopencm3/stm32/f4/adc.h>
 #include <stdio.h>
 
+#include "delay.h"
 #include "i2c_init.h"
 #include "lcd1602.h"
 #include "soil.h"
-#include <libopencm3/stm32/f4/rcc.h>
-#include <libopencm3/stm32/f4/adc.h>
-#include <libopencm3/stm32/f4/gpio.h>
+#include "send_log.h"
+
+#define GREEN_LED GPIO12
+#define BUFFER_SIZE 64
+
 
 static void clock_setup(void) {
     rcc_osc_on(RCC_HSE);
@@ -18,6 +17,8 @@ static void clock_setup(void) {
 
     rcc_periph_clock_enable(RCC_GPIOB);
     rcc_periph_clock_enable(RCC_I2C1);
+    rcc_periph_clock_enable(RCC_GPIOA);
+    rcc_periph_clock_enable(RCC_USART2);
 }
 
 void adc_setup(void) {
@@ -35,14 +36,29 @@ void adc_setup(void) {
 
     adc_set_sample_time(ADC1, ADC_CHANNEL0, ADC_SMPR_SMP_28CYC);
     adc_power_on(ADC1);
-    for (volatile int i = 0; i < 100000; i++);
+    dwt_delay_us(10);
 }
+void gpio_setup(void) {
+    gpio_mode_setup(GPIOB, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO10);
+    gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO8 | GPIO9);
+    gpio_set_output_options(GPIOB, GPIO_OTYPE_OD, GPIO_OSPEED_50MHZ, GPIO8 | GPIO9);
+    gpio_set_af(GPIOB, GPIO_AF4, GPIO8 | GPIO9);
+
+    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2 | GPIO3);
+    gpio_set_af(GPIOA, GPIO_AF7, GPIO2 | GPIO3);
+
+    gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GREEN_LED);
+    gpio_clear(GPIOD, GREEN_LED);
+
+}   
 
 int main(void) {
     clock_setup();
     gpio_setup();
+    usart_setup();
     i2c_setup();
     adc_setup();
+    dwt_setup();
 
     lcd1602_setup(I2C1);
 
@@ -58,13 +74,15 @@ int main(void) {
         sprintf(str, "%u", humidity_level);
         sprintf(third, "%s%s", fstr, str);
         sprintf(third, "%s%s", third, "%");
-        if (wet_or_dry == 0) {
+        if (wet_or_dry == 1) {
             lcd1602_print_string(I2C1, LCD1602_LINE_1, "It's wet!!!");
             lcd1602_print_string(I2C1, LCD1602_LINE_2, third);
         } else {
             lcd1602_print_string(I2C1, LCD1602_LINE_1, "It's dry!!!");
             lcd1602_print_string(I2C1, LCD1602_LINE_2, third);
         }
+        usart_transmit(third);
+        dwt_delay_ms(10000);
     }
 }
 
