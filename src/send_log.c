e3e19c322e89d7b64944caca1fe01105cd985861
RenChan18 <renya.talacheva@gmail.com>
2025-04-26 12:25:04 +0300

    a prototype for generating a data log has been implemented. sending the log to the queue has been implemented

--- src/send_log.c
+++ src/send_log.c
@@ -1,3 +1,48 @@
-#include "send_log.h"
+#include "app_log.h"
+
+void create_log(char *buf, size_t buf_size, DataSoil_t curr_humidity) {
+    const char prefix[] = "[START_INFO] 2025-04-26 14:30:34 | Humidity is:";
+    const char postfix[] = "\%[END_INFO]\r\n";
+
+    size_t prefix_len = strlen(prefix);
+    size_t postfix_len = strlen(postfix);
+
+    memcpy(buf, prefix, prefix_len);
+    buf+= prefix_len;
+    buf_size -= prefix_len;
+
+    if (curr_humidity.humidity_lvl == 0) {
+        *buf++ = '0';
+    } else {
+        char tmp[10];
+        int i = 0;
+        while (num > 0 && sizeof(tmp)-1) {
+            tmp[i++] = '0' + (num % 10);
+            num /= 10;
+        }
+        while (i-- > 0 && i < sizeof(tmp)-1) {
+            *buf++ = tmp[i];
+            buf_size--;
+        }
+    }
+    memcpy(buf, postfix, postfix_len);
+    buf+= postfix_len;
+    buf_size -= postfix_len;
+    *buf = '\0';
+}
+
+void send_log_queue(const char *log) {
+    for (; *log; ++log) {
+        while (xQueueSend(xLogData, log, pdMS_TO_TICKS(100)) != pdPASS) {
+            vTaskDelay(1);
+        }
+    }
+
+}
+
+
+
+
+
 
 
