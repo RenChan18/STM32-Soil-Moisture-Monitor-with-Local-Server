4f3e1c653cd6d31c62dff3eddb5b1c8db18af36b
RenChan18 <renya.talacheva@gmail.com>
2025-05-05 21:38:59 +0300

    the functionality has been transferred to the use of the structure. debugging outputs have been removed

--- src/log_engine.c
+++ src/log_engine.c
@@ -6,11 +6,13 @@ QueueHandle_t xHumData = NULL;
 
 static void scan_solid_task(void *unused) {
     (void)unused;
+     vTaskDelay(pdMS_TO_TICKS(1));
     DataSoil_t actual_data = {0,0};
-
     while(1) {
         actual_data.is_wet = read_soil_digital();
         actual_data.humidity_lvl = to_percent(read_soil_analog());
+        vTaskDelay(100);
+        gpio_toggle(GPIOD, GREEN_LED);
         while (xQueueSend(xHumData, &actual_data, pdMS_TO_TICKS(100)) != pdPASS) {
             vTaskDelay(1);
         }
@@ -21,13 +23,13 @@ static void logger_task(void *unused) {
     (void)unused;
     char log[BUFFER_SIZE] = {0};
     DataSoil_t received_data;
-
     while(1) {
-        if (xQueueReceive(xHumData, &received_data, pdMS_TO_TICKS(100)) == pdPASS) {
+        if (xQueueReceive(xHumData, &received_data, portMAX_DELAY) == pdPASS) {
             memset(log, 0, BUFFER_SIZE);
+            gpio_toggle(GPIOD, ORANGE_LED);
             create_log(log, BUFFER_SIZE, received_data);
             send_log_queue(log);
-            vTaskDelay(pdMS_TO_TICKS(LOG_DELAY_MS));
+            vTaskDelay(pdMS_TO_TICKS(20));
         }
     }
 }
@@ -37,30 +39,33 @@ static void usart_tx_task(void *unused) {
     char c;
     while(1) {
         if(xQueueReceive(xLogData, &c, portMAX_DELAY) == pdPASS) {
-            while (!usart_get_flag(USART2, USART_SR_TXE));
-            usart_send(USART2, c);
+            uint32_t timeout = 100000;
+            while (!usart_get_flag(USART2, USART_SR_TXE) && -- timeout){
+                vTaskDelay(1);
+            }
+            if (timeout)
+                usart_send(USART2, c);
         }
     }
 }
-
 void start_app(void) {
-    // Создаем очереди с проверкой
+
     xHumData = xQueueCreate(32, sizeof(DataSoil_t));
-    xLogData = xQueueCreate(128, sizeof(char));  // Для USART достаточно 128 символов
+    xLogData = xQueueCreate(128, sizeof(char));  
     
-    /*if (xHumData == NULL) {
-    printf("ERROR: xHumData NULL\r\n");
-    while(1);
+    if (xHumData == NULL) {
+        while(1);
     }
     if (xLogData == NULL) {
-    printf("ERROR: xLogData NULL\r\n");
-    while(1);
-    }*/
-    // Увеличиваем размер стека
-    xTaskCreate(scan_solid_task, "scan_solid", 128, NULL, tskIDLE_PRIORITY+4, NULL);
-    xTaskCreate(logger_task, "logger", 128, NULL, tskIDLE_PRIORITY+2, NULL);
-    xTaskCreate(usart_tx_task, "uart_tx", 128, NULL, tskIDLE_PRIORITY+1, NULL);
+        while(1);
+    }
+
+    xTaskCreate(scan_solid_task, "scan_solid", 512, NULL, tskIDLE_PRIORITY+2, NULL);
+    xTaskCreate(logger_task, "logger", 512, NULL, tskIDLE_PRIORITY+2, NULL);
+    xTaskCreate(usart_tx_task, "uart_tx", 512, NULL, tskIDLE_PRIORITY+1, NULL);
     
+    gpio_clear(GPIOD, GREEN_LED | ORANGE_LED);
+
     vTaskStartScheduler();
 }
 
